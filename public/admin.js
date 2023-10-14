baf9f335961f193fee1d1b5eb715885203feed08
Lucia Sotelo <luciasotelo@Lucias-MacBook-Pro.local>
2023-10-14 13:16:51 -0700

    assignment complete

--- public/admin.js
+++ public/admin.js
@@ -1,2 +1,44 @@
 
-// Your Code Here
+async function main() {
+
+    let response = await fetch('http://localhost:3001/listBooks')
+    let books = await response.json()
+
+    books.forEach(renderBookForm)
+}
+
+function renderBookForm(book) {
+    let bookForm = document.querySelector('.container')
+ // creating a list item -setting text for li to be the book title
+    let bookLi = document.createElement("li")
+    bookLi.textContent = book.title
+// creating a textbox, we used the input element and setting value into book quantity
+    let bookQuantity = document.createElement("input")
+    bookQuantity.value = book.quantity
+//    append the text box (input element we created) to the li
+    bookLi.append(bookQuantity)
+
+    let saveButton = document.createElement('button')
+    saveButton.textContent = 'SAVE'
+
+    // adding an event listener to save button
+    saveButton.addEventListener('click', () => {
+        let response = fetch('http://localhost:3001/updateBook',{
+            method: 'PATCH',
+            headers: {'Content-Type':'application/json'},
+            body: JSON.stringify({
+                id: book.id, 
+                quantity: bookQuantity.value
+
+            })
+        })
+    })
+    bookLi.append(saveButton)
+// append li to the book form div container we have.
+    bookForm.append(bookLi)
+
+   0 
+    
+}
+
+main()
